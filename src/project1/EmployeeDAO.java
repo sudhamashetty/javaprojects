91d3a754c1adf07f640bc1a7e55a4c857e651268
Sumanth Naik <sumanthnaik02@gmail.com>
2025-11-28 13:47:33 +0530

    added my sumanth

--- src/project1/EmployeeDAO.java
+++ src/project1/EmployeeDAO.java
@@ -1,18 +1,89 @@
-//package project1;
-//
-//import java.sql.*;
-//public class EmployeeDAO {
-////insert operation
-//    public static void addEmployees(Employee emp) {
-//        String sql="INSERT INTO Employees(name,department,salary,email,phone) VALUES (?,?,?,?,?)";
-//                try(Connection connect= Databaseconnection.connect();
-//                    PreparedStatement stmt= connect.prepareStatement(sql)) {
-//                    stmt.setString(1,emp.getName());
-//                    stmt.setString(2, emp.getDepartment());
-//                    stmt.setDouble(3,emp.getSalary());
-//                    stmt.setString(4,emp.getDepartment());
-//                    stmt.setString(5,emp.getPhone());
-//
-//        }
-//    }
-//}
+package project1;
+
+import project1.Databaseconnection;
+
+import java.sql.*;
+
+public class EmployeeDAO {
+
+    //insert operation
+    public static void addEmployees(Employee emp) {
+        String sql = "INSERT INTO Employees(name,department,salary,email,phone) VALUES(?,?,?,?,?)";
+
+        try (Connection connect = Databaseconnection.connect();
+             PreparedStatement stmt = connect.prepareStatement(sql)) {
+
+            stmt.setString(1, emp.getName());
+            stmt.setString(2, emp.getDepartment());
+            stmt.setDouble(3, emp.getSalary());
+            stmt.setString(4, emp.getEmail());
+            ;
+            stmt.setString(5, emp.getPhone());
+
+            int rowsInserted = stmt.executeUpdate();
+            if (rowsInserted > 0) {
+                System.out.println("Employee added successfully!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //view operation
+    public static void viewEmployees() {
+        String sql = "SELECT * FROM Employees";
+
+        try (Connection connect = Databaseconnection.connect();
+             Statement stmt = connect.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                System.out.println(rs.getInt("emp_id") + " | " +
+                        rs.getString("name") + " | " +
+                        rs.getString("department") + " | " +
+                        rs.getDouble("salary") + " | " +
+                        rs.getString("email") + " | " +
+                        rs.getString("phone"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //update operation
+    public static void updateEmployee(int empId, double newSalary) {
+        String sql = "UPDATE Employees SET salary= ? WHERE emp_id=?";
+
+        try (Connection connect = Databaseconnection.connect();
+             PreparedStatement stmt = connect.prepareStatement(sql)) {
+
+            stmt.setDouble(1, newSalary);
+            stmt.setInt(2, empId);
+
+            int rowsUpdated = stmt.executeUpdate();
+            if (rowsUpdated > 0) {
+                System.out.println("Employee salary updated successfully!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //delete operation
+    public static void deleteEmployee(int empId) {
+        String sql = "DELETE FROM Employees WHERE emp_id=?";
+
+        try (Connection connect = Databaseconnection.connect();
+             PreparedStatement stmt = connect.prepareStatement(sql)) {
+
+            stmt.setInt(1, empId);
+
+            int rowsDeleted = stmt.executeUpdate();
+            if (rowsDeleted > 0) {
+                System.out.println("Employee deleted successfully!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/project1/Main.java
+++ src/project1/Main.java
@@ -1,17 +1,58 @@
 package project1;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            System.out.println("\nEmployee Management System");
+            System.out.println("1. Add Employee");
+            System.out.println("2. View Employees");
+            System.out.println("3. Update Employee salary");
+            System.out.println("4. Delete Employee");
+            System.out.println("5. Exit");
+            System.out.println("Choose an option: ");
+            int choice =sc.nextInt();
+            switch(choice){
+                case 1:
+                    System.out.println("Enter Name: ");
+                    String name = sc.next();
+                    System.out.println("Enter Department: ");
+                    String department = sc.next();
+                    System.out.println("Enter Salary: ");
+                    double salary = sc.nextDouble();
+                    System.out.println("Enter Email: ");
+                    String email = sc.next();
+                    System.out.println("Enter phone: ");
+                    String phone = sc.next();
+                    EmployeeDAO.addEmployees(new Employee(0,name,department,salary,email,phone));
+                    break;
+
+                case 2:
+                    EmployeeDAO.viewEmployees();
+                    break;
+
+                case 3:
+                    System.out.println("Enter Employee ID: ");
+                    int empId = sc.nextInt();
+                    System.out.println("Enter the New Salary: ");
+                    double newSalary = sc.nextDouble();
+                    EmployeeDAO.updateEmployee(empId,newSalary);
+                    break;
+
+                case 4:
+                    System.out.println("Enter Employee ID to Delete: ");
+                    int empid = sc.nextInt();
+                    EmployeeDAO.deleteEmployee(empid);
+                    break;
+
+                case 5:
+                    System.out.println("Exiting........");
+                    sc.close();
+                    return;
+            }
         }
     }
 }
